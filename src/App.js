b25b69af1bc2b2f005677416b36be8840cc5ba16
Boris K. Nekezov <boris.nekezov@gmail.com>
2021-04-19 16:39:40 +0300

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// $6604
+import HomePage from './homepage.component';
+
+function App() {
+	return (
+		<div>
+			{/*// $6605 */}
+			<HomePage />
+		</div>
+	);
+}
+
+export default App;
